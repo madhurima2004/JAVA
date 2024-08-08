64c507ffd016e61b9a3c4a596cec606b8dc66f7a
Madhurima Saha <it.20213057@gmail.com>
2024-08-08 20:53:51 +0530

    commit

---
+++ maxnum.java
@@ -0,0 +1,21 @@
+package JAVA;
+class Arrayex{
+    void maxOfArray(){
+        int[] arr={0,1,73,8,53,};
+        int ans=0;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>ans){
+                ans=arr[i];
+            }
+        }
+        System.out.println(ans);
+    }
+}
+public class maxnum {
+
+    public static void main(String[] args) {
+        Arrayex obj = new Arrayex();
+        obj.maxOfArray();
+        
+    }
+}
