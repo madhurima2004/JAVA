34f688dacb0b5622ba00c6e2eedd2617f2f22eaf
Madhurima Saha <it.20213057@gmail.com>
2024-08-10 21:29:08 +0530

    added new files

---
+++ Binary_to_Decimal.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Binary_to_Decimal {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int binary_num=sc.nextInt();
+        int ans=0;
+        int power=1; // 2^0=1 power of 2
+        while(binary_num>0){
+            int unit_dig=binary_num%10;
+            ans = ans+(unit_dig*power);
+            binary_num/=10;
+            power*=2;
+        }
+        System.out.println(ans);
+    }
+}
