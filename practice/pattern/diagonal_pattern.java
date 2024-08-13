2439a7bcb3fe6a7ebd7f18ed82ceef3964d6c5e6
Madhurima Saha <it.20213057@gmail.com>
2024-08-13 11:48:44 +0530

    commit

---
+++ practice/pattern/diagonal_pattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class diagonal_pattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter row number :");
+        int r = sc.nextInt();
+
+        for(int i=1;i<=r;i++){
+            for(int j=1;j<=i;j++){
+                if(i>=2 && j<=i-1){
+                    System.out.print(" ");
+                }
+                else{
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
