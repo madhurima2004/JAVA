e37b2d302108270018e86838050dcb725ec85c49
Madhurima Saha <it.20213057@gmail.com>
2024-08-15 21:24:23 +0530

    commit

--- practice/pattern/Right_align_triangle.java
+++ practice/pattern/Right_align_triangle.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Right_align_triangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int r= sc.nextInt();
-        for(int i=1;i<=r;i++){
-            for(int j=r-1;j>=i;j--){
+        int row= sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=row-1;j>=i;j--){
                 System.out.print(" ");
             }
             for(int k=1;k<=i;k++){
