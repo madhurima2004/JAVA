ca8113e3d6764f106622d565feb199d3697f52d6
Madhurima Saha <it.20213057@gmail.com>
2024-08-16 22:31:12 +0530

    commit

--- practice/String/palindrom.java
+++ practice/String/palindrom.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class palindrom {
-    
+    public static void main(String[] args) {
+        String sn = "";
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a String:");
+        String s= sc.next();
+}
 }
