ff68466b9bcb818a39058108958778651a312492
Madhurima Saha <it.20213057@gmail.com>
2024-08-17 20:17:21 +0530

    commit

--- practice/String/palindrom.java
+++ practice/String/palindrom.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 
 public class palindrom {
-    public static void main(String[] args) {
-        String sn = "";
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {   
+     Scanner sc= new Scanner(System.in);
         System.out.println("Enter a String:");
-        String str= sc.next();
-        if (isPalindrome(str))
-            System.out.print("It is a palindrome");
-        else
-            System.out.print("It is not a palindrome");
+        String s= sc.next();
+        // System.out.println(s);
+        String rev="";
+        for(int i=s.length()-1;i>=0;i--){
+            rev=rev+s.charAt(i);
+        }
+        if(s.equals(rev)){
+            System.out.println("PALINDROM");
+        }
+        else{
+            System.out.println("NOT PALINDROM");
+        }
+        
 }
-}
-static boolean isPalindrome(String str)
-{
-
-    int i = 0, j = str.length() - 1;
-    while (i < j) {
-        if (str.charAt(i) != str.charAt(j))
-            return false;
-        i++;
-        j--;
-    }
-    return true;
 }
