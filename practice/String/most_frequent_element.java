31b01e2b8837dbd509cd59b3224954ed7a2f86e1
Madhurima Saha <it.20213057@gmail.com>
2024-08-19 22:16:45 +0530

    commit

--- practice/String/most_frequent_element.java
+++ practice/String/most_frequent_element.java
@@ -1,6 +1,28 @@
-import java.util.Scanner;
+import java.util.HashMap;;
 public class most_frequent_element {
-    Scanner sc= new Scanner(System.in);
-    System.out.println("Enter a String:");
-    String s= sc.next();
+    public static void main(String[] args) {
+        int arr[]={2,4,1,6,2,2,2,5,7,8,9};
+        int ans = mostFrequentElement(arr);
+        System.out.println("answer :"+ ans);
+    }
+    public static int mostFrequentElement(int arr[]){
+        HashMap<Integer,Integer> freqMap = new HashMap<>();
+        int ans=-1;
+        int maxfrequency =0;
+        for(int i=0;i<arr.length;i++){
+            if(freqMap.containsKey(arr[i])){
+                int val = freqMap.get(arr[i]);
+                val+=1;
+                freqMap.put(arr[i],val);
+            }
+            else{
+                freqMap.put(arr[i],1);
+            }
+            if(freqMap.get(arr[i])>maxfrequency){
+                maxfrequency =freqMap.get(arr[i]);
+                ans=arr[i];
+            }
+        }
+        return ans;
+    }
 }
