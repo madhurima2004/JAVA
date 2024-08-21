e700a19bb05a1146861a4dc7247aa195a78f3fd7
Madhurima Saha <it.20213057@gmail.com>
2024-08-21 20:07:16 +0530

    commit

---
+++ practice/Arrays/count_elem.java
@@ -0,0 +1,21 @@
+// package Arrays;
+
+import java.util.Scanner;
+
+public class count_elem {
+
+static int count_occurances(int[] arr){
+
+}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter "+n+"elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+    }
+}
