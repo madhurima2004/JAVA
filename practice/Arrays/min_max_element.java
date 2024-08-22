de251073f779823c02943c6febf6e364dae2b6ba
Madhurima Saha <it.20213057@gmail.com>
2024-08-22 20:12:15 +0530

    commit

---
+++ practice/Arrays/min_max_element.java
@@ -0,0 +1,28 @@
+// package Arrays;
+
+import java.util.Scanner;
+import java.util.Arrays;
+public class min_max_element {
+    
+static int[] newArray(int[] arr){
+        Arrays.sort(arr);
+        int[] ans ={arr[0],arr[arr.length-1]};
+        return ans;
+
+}
+
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("enter " + n + "elements:");
+        for(int i=0;i<arr.length;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] ans = newArray(arr);
+        // System.out.println(newArray(arr));
+        System.out.println("smallest " + ans[0]);
+        System.out.println("largest " + ans[1]);
+}
+}
