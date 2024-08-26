b0c08857384b6f326473480719306ea642f294da
Madhurima Saha <it.20213057@gmail.com>
2024-08-26 22:18:21 +0530

    commit

--- practice/Arrays/Reverse.java
+++ practice/Arrays/Reverse.java
@@ -1,3 +1,25 @@
 public class Reverse {
-    
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5, 6, 7};
+
+        reverseArray(array);
+
+        
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static void reverseArray(int[] array) {
+        int left = 0;                
+        int right = array.length - 1; 
+
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
+        }
+    }
 }
