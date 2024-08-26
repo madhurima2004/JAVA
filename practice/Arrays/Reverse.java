f36e59bdc4571056516a6e35bcae9a7e4fc404c1
Madhurima Saha <it.20213057@gmail.com>
2024-08-26 22:19:20 +0530

    commit

--- practice/Arrays/Reverse.java
+++ practice/Arrays/Reverse.java
@@ -1,6 +1,6 @@
 public class Reverse {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7};
+        int[] array = {1, 2, 3, 4, 5, 6, 7,8,10};
 
         reverseArray(array);
 
