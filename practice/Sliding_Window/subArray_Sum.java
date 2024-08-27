30bc83dff3e78064b153672fe4ba63d171d25a16
Madhurima Saha <it.20213057@gmail.com>
2024-08-27 20:03:07 +0530

    commit

--- practice/Sliding_Window/subArray_Sum.java
+++ practice/Sliding_Window/subArray_Sum.java
@@ -1,5 +1,17 @@
 public class subArray_Sum {
     public static void main(String[] args) {
-        
+        int [] arr={18,4,23,67,54};
+        int k=3;
+        int sum=0;
+        int n = arr.length;
+        for(int i=0;i<k;i++){
+            sum=sum+arr[i];
+
+        }
+        System.out.println(sum);
+        for(int i=1;i<=n-k;i++){
+            sum=sum=arr[i-1]+arr[i+k-1];
+            System.out.println(sum);
+        }
     }
 }
