06eef5aefed456a1a92957cec36eec7c3902f281
Madhurima Saha <it.20213057@gmail.com>
2024-09-04 23:20:23 +0530

    commit

---
+++ practice/String/string_encoding.java
@@ -0,0 +1,10 @@
+//1.  replace vowel to % in 1st word
+//2.  replace consonent to # in 2nd word
+//3. upercase in 3rd 
+public class string_encoding {
+public static void main(String[] args) {
+    String str = "How are You?";
+    
+
+}
+}
